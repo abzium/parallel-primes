38b15108a8917c692c12e73922891037f8cdc760
Abdul <muizabdul216@gmail.com>
2024-01-24 16:54:25 -0500

    is_prime optimization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+import time, math
 
 def is_prime(n):
     ''' Check whether a given number is prime '''
@@ -6,14 +6,14 @@ def is_prime(n):
     if n == 1:
         return False
     
-    for i in range(2, n): 
+    for i in range(2, math.isqrt(n)): # can't have both factors higher than sqrt(n)
         if n % i == 0:
             return False
         
     return True
 
 start_time = time.time()
-n = 100000
+n = 1000000
 sum = 0
 primes_found = 0
 for i in range(2, n):
@@ -22,6 +22,3 @@ for i in range(2, n):
         primes_found += 1
 
 print(f"{time.time() - start_time} {primes_found} {sum}")
-
-
-
