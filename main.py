59e0fa2cda469f7496c9ba3b7f6d0361d251b8ce
Abdul <muizabdul216@gmail.com>
2024-01-24 19:23:31 -0500

    better and correct is_prime

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import time, math
 def is_prime(n):
     ''' Check whether a given number is prime '''
 
-    if n == 1:
+    if n <= 1:
         return False
     
-    for i in range(2, math.isqrt(n)): # can't have both factors higher than sqrt(n)
+    if n == 2:
+        return True
+    
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, math.isqrt(n) + 1, 2):
         if n % i == 0:
             return False
         
@@ -20,5 +26,7 @@ for i in range(2, n):
     if is_prime(i):
         sum += i
         primes_found += 1
+        
 
 print(f"{time.time() - start_time} {primes_found} {sum}")
+print(is_prime(4))
